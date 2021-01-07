88469d1512118ddf9fd6fdae7c6bbaa97cd5719f
Lorena Coelho <lorenagfcoelho@gmail.com>
2021-01-07 12:38:14 -0300

    Criar promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -8,4 +8,25 @@ class PromotionsController < ApplicationController
         @promotion = Promotion.find(params[:id])
     end
 
+    def new
+        @promotion = Promotion.new
+    end
+
+    def create
+        @promotion = Promotion.create(promotion_params)
+        redirect_to @promotion
+    end
+
+    private
+
+    def promotion_params
+        params.require(:promotion)
+              .permit(:name,
+                      :description,
+                      :code,
+                      :discount_rate,
+                      :coupon_quantity,
+                      :expiration_date)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :promotions, only: %i[index show]
+  resources :promotions, only: %i[index show new create]
 end
