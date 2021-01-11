593bbf2f01a791dd6a61126a1f61d1217b2b9b69
Lorena Coelho <lorenagfcoelho@gmail.com>
2021-01-11 13:24:51 -0300

    Mudança para pt-BR e WIP testes

---
+++ spec/features/user_log_in_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'User log in' do
+    scenario 'and receive welcome message' do
+        user = User.create!(email: 'fulana@locaweb.com.br', password:'123456')
+    
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: user.email
+        fill_in 'Senha', with: user.password
+        click_on 'Entrar'
+
+        expect(current_path).to eq(root_path)
+        #expect(page).to have_content('Login efetuado com sucesso')
+        expect(page).to have_content(user.email)
+        expect(page).to_not have_link('Login')
+        expect(page).to have_link('Sair')
+        #mostra links quando logado
+    end
+
+    #continuar
+    xscenario 'and log out' do
+        user = User.create!(email: 'fulana@locaweb.com.br', password:'123456')
+    
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: user.email
+        fill_in 'Senha', with: user.password
+        click_on 'Entrar'
+        click_on 'Sair'
+
+        expect(current_path).to eq(root_path)
+        expect(page).to_not have_content(user.email)
+        expect(page).to have_link('Login')
+        expect(page).to_not have_link('Sair')
+    end
+
+    #registro
+end
